6449b2ef0fe4b86ae9d47ea49f7054014ba20c54
Rafay Khan <rafaykhan3012@yahoo.com>
2023-06-08 23:24:51 +0500

    change

--- src/index.ts
+++ src/index.ts
@@ -16,9 +16,6 @@ console.log(charactersCollection.data);
  linkedlist.add(-10)
  linkedlist.add(-3)
  linkedlist.add(4)
- 
 
-// const sorter = new Sorter(linkedlist);
-// sorter.sort();
 
 linkedlist.print()
